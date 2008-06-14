b125bdb21983e090dd243e849229f7be4f373bbc
kindly <kindly>
2008-06-14 00:37:07 +0000

    movement

---
+++ manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import os, sys
+
+
+
+def create():
+
+    import boot_toby
+
+def run():
+    import http
+    if os.environ.get("REQUEST_METHOD", ""):
+        from wsgiref.handlers import BaseCGIHandler
+        BaseCGIHandler(sys.stdin, sys.stdout, sys.stderr, os.environ).run(http.app)
+    else:
+        from wsgiref.simple_server import WSGIServer, WSGIRequestHandler
+        httpd = WSGIServer(('', 8000), WSGIRequestHandler)
+        httpd.set_app(http.app)
+        print "Serving HTTP on %s port %s ..." % httpd.socket.getsockname()
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+   if 'create' in sys.argv:
+        create()
+   if 'run' in sys.argv:
+        run()
+
