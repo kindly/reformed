97d46b3428a048eebc701b77ccd3eb15f9b8a5d4
toby.junk <toby.junk>
2008-06-16 23:51:45 +0000

    added ability to serve static content

--- http.py
+++ http.py
@@ -3,16 +3,21 @@ import wsgiref.util
 import form
 import string
 import time, datetime
-#import model
 import cgi
 import reformed
 
+# for static
+import os, os.path
+import posixpath
+import mimetypes
+from wsgiref.util import FileWrapper
 
 # get our data up
 reformed.data = reformed.Database()
 reformed.data.create_tables()
 
 uptime = datetime.datetime.now()
+
 
 @session()
 def app(environ, start_response):
@@ -47,6 +52,10 @@ def app(environ, start_response):
 	elif cmd == 'logout':
 		clear_authentication(session)
 		body = "logged out"
+	elif cmd == 'content':
+		# static content
+		path = path_info[2]
+		return(static(environ, start_response, path))
 	else:
 		body = 'lost'
 
@@ -60,12 +69,12 @@ def app(environ, start_response):
 		body += "\n\n<p><small>page created in %s seconds - uptime %s</small></p>" % (time.clock() - start_time, datetime.datetime.now() - uptime)
 		# HTTP header
 		start_response('200 OK', [('Content-Type', 'text/html')])
-		return body
+		return [html_header + body + html_footer]
 		
 	elif redirect:
 		# HTTP header
 		start_response('200 OK', [('Content-Type', 'text/html')])
-		return "<a href='%s'>%s</a>" % (redirect, redirect)
+		return ["<a href='%s'>%s</a>"] % (redirect, redirect)
 	else:
 		print "nothing to do" # FIXME we want to throw an error here
 
@@ -93,3 +102,57 @@ def clear_authentication(http_session):
 	http_session['username'] = None
 	http_session['user_id'] = None
 
+
+
+
+
+def static(environ, start_response, path):
+	path="/home/toby/reformed/content/%s" % path
+	print path
+	if os.path.isfile(path):
+		stat = os.stat(path)
+		mimetype = mimetypes.guess_type(path)[0] or 'application/octet-stream'
+		max_age = None # FIXME: not done
+		last_modified = stat.st_mtime
+		not_modified = False
+		if 'HTTP_IF_MODIFIED_SINCE' in environ:
+			value = parse_date_timestamp(environ['HTTP_IF_MODIFIED_SINCE'])
+			if value >= last_modified:
+				not_modified = True
+		etag = '%s-%s' % (last_modified, hash(path))
+		if 'HTTP_IF_NONE_MATCH' in environ:
+			value = environ['HTTP_IF_NONE_MATCH'].strip('"')
+			if value == etag:
+				not_modified = True
+		headers = [
+			('Content-Type', mimetype),
+			('Content-Length', str(stat.st_size)),
+			('ETag', etag),
+	## FIXME           ('Last-Modified', serialize_date(last_modified))
+	]
+		if max_age: 
+			headers.append(('Cache-control', 'max-age=%s' % max_age))
+		if not_modified:
+			start_response('304 Not Modified', headers)
+			return []
+		else:
+			print path
+			start_response('200 OK', headers)
+			f = open(path, 'rb')
+			return FileWrapper(f)
+	else:
+		start_response('404 Not Found', [])
+		return []
+
+# dirty old hack
+html_header = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<title>Reformed - what a horrid name</title>
+<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1' />
+<link href='/content/default.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<h1><img src="/content/reformed.png" title="cool" alt="reformed" /></h1>\n"""
+
+html_footer = "\n</body>\n</html>"
