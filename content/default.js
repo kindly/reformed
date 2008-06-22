b586775cdf6a883b1237887303924fb180ca7a12
toby.junk <toby.junk>
2008-06-22 10:56:07 +0000

    couple of updates I forgot last night
    js now gets 'This' rather than the direct prefix name for shorter/easier html

--- content/default.js
+++ content/default.js
@@ -1,5 +1,7 @@
 function select_box(arg){
-	x=document.getElementById(arg + '_::_selected');
+	arg = String(arg.name);
+	arg = arg.substring(0,arg.lastIndexOf(":"));
+	x=document.getElementById(arg + ':_::_selected');
 	if (x){
 		x.checked=true;
 	}
