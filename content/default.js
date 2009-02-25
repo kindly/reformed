fa8c94925852ac2abc0922ef681e5bf191115754
kindly <kindly@gmail.com>
2009-02-25 20:51:00 +0000

    delete all old

--- content/default.js
+++
@@ -1,8 +0,0 @@
-function select_box(arg){
-	arg = String(arg.name);
-	arg = arg.substring(0,arg.lastIndexOf(":"));
-	x=document.getElementById(arg + ':_::_selected');
-	if (x){
-		x.checked=true;
-	}
-}
