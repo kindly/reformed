8fd2c950501c7ff60f2834340a3a10e7ab30c6d3
tobes <toby.junk@gmail.com>
2009-06-03 23:52:09 +0100

    add file header

--- content/reformed.js
+++ content/reformed.js
@@ -1,5 +1,36 @@
+/*
 
+    This file is part of Reformed.
 
+    Reformed is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as 
+    published by the Free Software Foundation.
+
+    Reformed is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Reformed.  If not, see <http://www.gnu.org/licenses/>.
+
+	-----------------------------------------------------------------
+	
+	Reformed
+	Copyright (c) 2008-2009 Toby Dacre & David Raznick
+	
+	reformed.js
+	======
+	
+	A place for lost stuff at the moment but it will eventually hold the 
+	core functionality eg initialisation
+
+	$REFORMED
+
+	Public Functions
+	================
+	
+*/
 
 // this calls init() onload
 $(init);
