7df1d75f4f70d88bbc83acdb928cf1650db0d357
tobes <toby.junk@gmail.com>
2010-01-12 18:41:52 +0000

    change node.js to use new form creation code

--- content/node.js
+++ content/node.js
@@ -1476,15 +1476,11 @@ var fn = function(packet, job){
              form = $.Util.FormDataNormalize(form);
              data = packet.data.data.data;
              var paging = packet.data.data.paging;
-             if (form.params && form.params.form_type == 'grid'){
+             if (form.params.form_type == 'grid'){
                 $('#' + root).grid(form, data, paging);
-             } else if (form.params && form.params.form_type == 'normal') {
-                $('#' + root).form(form, data, paging);
              } else {
-                $('#' + root).html(node_generate_html(form, data, paging, root)).scrollTop(0);
-                form_setup(root, form);
+                $('#' + root).form(form, data, paging);
              }
-             $INFO.setState(root, 'node', packet.data.node);
              break;
          case 'save_error':
             data = packet.data.data;
