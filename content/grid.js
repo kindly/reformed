a45dbe968f06dcff0cbd9dc46ce91730a84117d8
tobes <toby.junk@gmail.com>
2010-01-15 13:29:55 +0000

    cache complex_control info

--- content/grid.js
+++ content/grid.js
@@ -358,10 +358,8 @@ $.Grid.Movement = function(input, form_data, grid_data){
     function make_editable(){
         // make the cell editable
         var $item = current.$item;
-        // is this a complex control?
-        var complex_control = (current.field.params && current.field.params.control == 'dropdown');
 
-        if (complex_control){
+        if (current.complex_control){
             $item = $item.find('div.data');
         }
 
@@ -369,7 +367,7 @@ $.Grid.Movement = function(input, form_data, grid_data){
         // if this is the first row we need to adjust the width to compensate for
         // any differences in the padding etc
         // don't do this for complex conrols as they do thier own wrapping
-        if (current.row === 0 && !complex_control){
+        if (current.row === 0 && !current.complex_control){
             current.$item.width(current.$item.width() - $.Util.Size.GRID_COL_EDIT_DIFF);
         }
         current.value = grid_data[current.row][current.field.name];
@@ -386,16 +384,15 @@ $.Grid.Movement = function(input, form_data, grid_data){
     function make_normal(){
         // return the item to it's normal state
         var $item = current.$item;
-        // is this a complex control?
-        var complex_control = (current.field.params && current.field.params.control == 'dropdown');
-        if (complex_control){
+
+        if (current.complex_control){
             $item = $item.find('div.data');
         }
         var value = util.make_normal($item, current.field);
         // if this is the first row we need to adjust the width to compensate for
         // any differences in the padding etc
         // don't do this for complex conrols as they do thier own wrapping
-        if (current.row === 0 && !complex_control){
+        if (current.row === 0 && !current.complex_control){
             current.$item.width(current.$item.width() + $.Util.Size.GRID_COL_EDIT_DIFF);
         }
         if (value === current.value){
@@ -512,6 +509,8 @@ $.Grid.Movement = function(input, form_data, grid_data){
             current.field = form_data.fields[col];
             current.$item = $new_item;
             current.col = col;
+
+            current.complex_control = (current.field.params && current.field.params.control == 'dropdown');
 
             // check cell is viewable
             make_cell_viewable()
@@ -762,7 +761,8 @@ $.Grid.Movement = function(input, form_data, grid_data){
         col : undefined,
         value : undefined,
         field : undefined,
-        $control : undefined
+        $control : undefined,
+        complex_control : undefined
     };
 
     var util = $.Util;
