50e0949c6f57fd7a0677875b1a365a7d31d6900e
tobes <toby.junk@gmail.com>
2010-02-11 02:24:16 +0000

    local.js for each application

---
+++ bug/content/local.js
@@ -0,0 +1,15 @@
+
+$.Buttons.action_hash = {
+    save: [['save', 'document-save.png', 'c', 'record'],[document, node_save, ['main','']]],
+    'delete':[['delete', 'edit-delete.png', 'd', 'record'],[document, node_delete, ['main','']]],
+    home: [['home', 'go-home.png', 'h', 'general'],[document, node_load, ['n:test.HomePage:']]],
+    new_ticket: [['new ticket', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.Ticket:new']]],
+    new_user: [['new user', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.User:new']]],
+    new_permission: [['new permission', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.Permission:new']]],
+    list_tickets: [['list tickets', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.Ticket:list']]]
+};
+
+
+$.Buttons.action_list = ['home',  'save', 'delete', 'new_ticket', 'list_tickets', 'new_permission', 'new_user'];
+
+
--- content/layout.js
+++ content/layout.js
@@ -32,14 +32,9 @@ $.fn.extend({
 });
 $.Buttons = {};
 
-$.Buttons.action_hash = {
-    save: [['save', 'document-save.png', 'c', 'record'],[document, node_save, ['main','']]],
-    'delete':[['delete', 'edit-delete.png', 'd', 'record'],[document, node_delete, ['main','']]],
-    home: [['home', 'go-home.png', 'h', 'general'],[document, node_load, ['n:test.HomePage:']]],
-    new_ticket: [['new ticket', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.Ticket:new']]],
-    new_user: [['new user', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.User:new']]],
-    list_tickets: [['list tickets', 'go-home.png', 'h', 'general'],[document, node_load, ['n:bug.Ticket:list']]]
-};
+$.Buttons.action_hash = {};
+
+$.Buttons.action_list = [];
 
 $.Buttons.action_call = function (action_name){
     // this function fires the event for action button clicks
@@ -110,7 +105,7 @@ $.LayoutManager = function () {
     
 
 
-        var action_list = ['home',  'save', 'delete', 'new_ticket', 'list_tickets', 'new_user'];
+        var action_list = $.Buttons.action_list;
 
         add_actions();
         $body.append($actions);
