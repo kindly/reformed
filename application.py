29638213edd7fb81c6df44df06f85b11eb490b04
tobes <toby.junk@gmail.com>
2010-04-28 22:52:51 +0100

    application.py add license info

--- application.py
+++ application.py
@@ -1,3 +1,24 @@
+##   This file is part of Reformed.
+##
+##   Reformed is free software: you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License version 2 as
+##   published by the Free Software Foundation.
+##
+##   Reformed is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with Reformed.  If not, see <http://www.gnu.org/licenses/>.
+##
+##   -----------------------------------------------------------------
+##
+##   Reformed
+##   Copyright (c) 2008-2010 Toby Dacre & David Raznick
+##
+
+
 import os
 import sys
 from sqlalchemy import MetaData, create_engine
