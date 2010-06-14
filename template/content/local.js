feeb20e6bb29dd7c9bf3fc22dae5dd40d356f61e
tobes <toby.junk@gmail.com>
2010-06-14 15:52:24 +0100

    template directory added

---
+++ template/content/local.js
@@ -0,0 +1,9 @@
+
+$.Buttons.action_hash = {
+    test1: ['Test', 'go-home.png', 'h', 'general', 'n:toby.Node1:'],
+    test2: ['Test', 'go-home.png', 'h', 'general', 'n:toby.Node2:'],
+};
+
+
+$.Buttons.action_list = ['test1', 'test2'];
+
