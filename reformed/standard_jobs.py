3adfdbb2a9ca138e88b06d69017b0834e744f02f
tobes <toby.junk@gmail.com>
2010-06-21 12:29:19 +0100

    standard jobs use application not database

--- reformed/standard_jobs.py
+++ reformed/standard_jobs.py
@@ -6,8 +6,9 @@ from data_loader import FlatFile
 
 class Messager(object):
 
-    def __init__(self, database, job_id):
-        self.database = database
+    def __init__(self, application, job_id):
+        self.application = application
+        self.database = application.database
         self.job_id = job_id
 
     def message(self, message, percent = None):
@@ -22,27 +23,28 @@ class Messager(object):
         util.load_local_data(self.database, row)
 
 
-def data_load_from_file(database, job_id, **kw):
+
+def data_load_from_file(application, job_id, **kw):
 
     table = kw.get("table")
     file = kw.get("file")
 
-    flatfile = FlatFile(database,
+    flatfile = FlatFile(application.database,
                         table,
                         util.get_dir(file))
 
-    messager = Messager(database, job_id)
+    messager = Messager(application, job_id)
 
     return flatfile.load(messager = messager)
 
 
-def wait(database, job_id, **kw):
+def wait(application, job_id, **kw):
 
     number = kw.get("time")
 
     time.sleep(int(number))
     return "%s is done!" % number
 
-def error(database, job_id, **kw):
+def error(application, job_id, **kw):
     number = kw.get("time")
     assert 1 <> 1
