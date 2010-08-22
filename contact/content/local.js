4b680d04a2c8ab3beacb30f9481daa09a9ab1454
kindly <kindly@gmail.com>
2010-08-23 00:45:53 +0100

    add contact app

---
+++ contact/content/local.js
@@ -0,0 +1,10 @@
+
+$.Buttons.action_hash = {
+    test1: ['New Person', '', '', 'general', 'n:new_person.NewPerson:'],
+    test2: ['Generate Data', '', '', 'general', 'n:new_person.DataGenerate:select:'],
+    test3: ['People', '', '', 'general', 'n:new_person.People:list:'],
+};
+
+
+$.Buttons.action_list = ['test1', 'test2', 'test3'];
+
