1eb0e7d0e2946b5770963e9f619379bcf276c42b
kindly <kindly@gmail.com>
2010-10-01 16:44:59 +0100

    test search added to new person

--- contact/custom_nodes/new_person.py
+++ contact/custom_nodes/new_person.py
@@ -2,6 +2,9 @@ from node import Node, TableNode, AutoForm, JobNode, AutoFormPlus
 from form import form
 from page_item import *
 from pprint import pprint
+from whoosh.query import Term, Or, And, Phrase
+import reformed.parsers as parsers
+import whoosh.analysis
 
 from global_session import global_session
 r = global_session.database
@@ -17,7 +20,7 @@ class NewPerson(Node):
         layout('spacer'), 
         layout('spacer'), 
 
-        button_box([['add', 'new_person.EvaluateDuplicate:_save:'],
+        button_box([['add', 'n:new_person.EvaluateDuplicate:_save:'],
                     ['cancel', 'BACK'],]
                    ),
         params =  {"form_type": "normal"},
@@ -33,7 +36,54 @@ class EvaluateDuplicate(Node):
     def call(self, node_token):
         data = node_token['main'].data
         #node_token.redirect('new_person.MakeContact:next:', node_data = data)
-        node_token.next_node('new_person.MakeContact', node_data = data, command = 'next')
+        node_token.general_error(self.get_results(data))
+        #node_token.next_node('n:new_person.MakeContact', node_data = data, command = 'next')
+
+    def get_results(self, data):
+
+        index = r.application.text_index
+        searcher = index.searcher()
+
+        sn = r.search_names
+        terms = []
+
+        names = data["name"].split()
+        communication = data["communication"]
+
+        for name in names:
+            terms.append(Term(sn["name"], name.lower()))
+
+        terms.append(Phrase(sn["name"], names))
+
+        email = parsers.email(communication)
+        phone_number = parsers.phonenumber(communication)
+        postcode = parsers.postcode(communication)
+        dob = parsers.date(communication)
+        ana = whoosh.analysis.StandardAnalyzer()
+
+        if email:
+            email_terms = [token.text for token in ana(email[1])]
+            for email_term in email_terms:
+                terms.append(Term(sn["email"], email_term))
+            terms.append(Phrase(sn["email"], email_terms, boost = 2))
+
+        if phone_number:
+            terms.append(Term(sn["number"], phone_number[1], boost = 2))
+        if dob:
+            terms.append(Term(sn["dob"], dob[1], boost = 2))
+        if postcode:
+            terms.append(Term(sn["postcode"], postcode[1], boost = 2))
+
+        query = Or(terms)
+
+        result = searcher.search(query)
+
+        return repr(query) + "\n\n" + "\n\n".join([repr(a) for a in result])
+
+
+
+
+        
 
 
 
