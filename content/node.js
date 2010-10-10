7edb653838b50aa6c385ba2c632265a789d5c408
tobes <toby.junk@gmail.com>
2010-10-10 21:45:57 +0100

    more code movement in javascript

--- content/node.js
+++ content/node.js
@@ -25,7 +25,7 @@
 */
 
 // JSLint directives
-/*global document setTimeout window */
+/*global document window */
 /*global $ REBASE console_log */
 
 
@@ -156,7 +156,6 @@ function change_user(user){
 
 REBASE.Node = function (){
 
-    var status_timer;
 
     /* Private functions. */
 
@@ -178,24 +177,6 @@ REBASE.Node = function (){
             }
         }
         return out;
-    }
-
-    function job_processor_status(data, node, root){
-        // display the message form if it exists
-        if (data.form){
-            $('#' + root).status_form();
-        }
-        // show info on form
-        if (data.data){
-            $('div.STATUS_FORM').data('command')('update', data.data);
-        }
-        // set data refresh if job not finished
-        if (!data.data || !data.data.end){
-            var node_string = "/:" + node + ":_status:id=" + data.data.id;
-            status_timer = setTimeout(function (){
-                                          load_node(node_string);
-                                      }, 1000);
-        }
     }
 
     function decode_node_string(node_string, item, target_form){
@@ -383,117 +364,6 @@ REBASE.Node = function (){
         get_node(decode);
     }
 
-    function process_node(packet, job){
-
-        var message;
-
-        if (packet.data === null){
-            console_log("NULL DATA PACKET");
-            return;
-        }
-
-        var root = 'main'; //FIXME
-
-        var title = packet.data.title;
-        if (title){
-            $.address.title(title);
-        }
-
-        var sent_node_data = packet.data.node_data;
-        if (sent_node_data){
-            global_node_data = sent_node_data;
-            global_current_node = packet.data.node;
-            console_log('node data:', global_node_data);
-        }
-
-        var user = packet.data.user;
-        if (user){
-            change_user(user);
-        }
-
-        var bookmark = packet.data.bookmark;
-        if (bookmark){
-           REBASE.Bookmark.process(bookmark);
-        }
-
-        var data;
-        switch (packet.data.action){
-            case 'redirect':
-                var link = packet.data.link;
-                if (link){
-                    switch (link){
-                        case 'BACK':
-                            window.history.back();
-                            break;
-                        case 'CLOSE':
-                            REBASE.Dialog.close();
-                            break;
-                        case 'RELOAD':
-                            REBASE.Dialog.close();
-                            load_node($.address.value());
-                            break;
-                        default:
-                            load_node('u:' + link);
-                            break;
-                    }
-                }
-                break;
-            case 'html':
-                $('#' + root).html(packet.data.data.html);
-                break;
-            case 'page':
-                //alert($.toJSON(packet.data.data));
-                $('#' + root).html(page_build(packet.data.data));
-                break;
-            case 'form':
-            case 'dialog':
-                 REBASE.Layout.update_layout(packet.data);
-                 break;
-            case 'function':
-                console_log('data', packet.data['function']);
-                REBASE.Functions.call(packet.data['function'], packet.data.data);
-                break;
-            case 'save_error':
-                data = packet.data.data;
-                // clear form items with no errors
-                break;
-            case 'save':
-                data = packet.data.data;
-                if (job && job.obj){
-                    // copy the obj_data that was saved with the job
-                    data.obj_data = job.obj_data;
-                    job.obj.data('command')('save_return', data);
-                } else {
-                    alert("we have not sent the object");
-                }
-                break;
-            case 'delete':
-                data = packet.data.data;
-                if (data.deleted){
-                    form_process_deleted(data.deleted);
-                }
-                break;
-            case 'general_error':
-                message = packet.data.data;
-                REBASE.Dialog.dialog('Error', message);
-                break;
-            case 'message':
-                message = packet.data.data;
-                REBASE.Dialog.dialog('Message', message);
-                break;
-            case 'forbidden':
-                message = 'You do not have the permissions to perform this action.';
-                REBASE.Dialog.dialog('Forbidden', message);
-                break;
-            case 'status':
-                job_processor_status(packet.data.data, packet.data.node, root);
-                break;
-            default:
-                REBASE.Dialog.dialog('Error', 'Action `' + packet.data.action + '` not recognised');
-                break;
-        }
-    }
-
 
 
     // exported functions
@@ -514,10 +384,6 @@ REBASE.Node = function (){
             // DO NOT USE THIS FUNCTION
             // Use load_node() instead
             get_node(decode);
-        },
-        'process' : function (packet, job){
-            // Called with returned data from the backend.
-            process_node(packet, job);
         }
     };
 }();
