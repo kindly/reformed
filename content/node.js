9c79250cc905b7d83bc7115c1c71076f03ac72c3
tobes <toby.junk@gmail.com>
2010-10-21 17:01:17 +0100

    fix search to use new node formaat

--- content/node.js
+++ content/node.js
@@ -34,7 +34,7 @@ var node_load;
 
 
 function search_box(){
-    var node = ':test.Search::q=' + $('#search').val();
+    var node = 'test.Search?q=' + $('#search').val();
     node_load(node);
     return false;
 }
