2dfe39c61bbd117e399d9bbacae48c7d67e2e08a
tobes <toby.junk@gmail.com>
2010-11-12 17:27:09 +0000

    make slider wider for usability

--- content/rebase.js
+++ content/rebase.js
@@ -641,7 +641,7 @@ REBASE.Interface = function (){
         add_logo();
 
         // set options for the panes
-        var layout_defaults = {spacing_open:3, spacing_close:6, padding:0, applyDefaultStyles:true};
+        var layout_defaults = {spacing_open:6, spacing_close:6, padding:0, applyDefaultStyles:true};
         var layout_north = {resizable:true, closable: false, slidable:false, spacing_open:0};
         var layout_west = {onresize : resize_main_pane, onopen : resize_main_pane, onclose : resize_main_pane};
 
